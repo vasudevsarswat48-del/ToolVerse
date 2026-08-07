7d1f2b6a7af17899962373807044cf64cdcc3eb9
vasudevsarswat48-del <vasudevsarswat48@gmail.com>
2026-08-07 11:22:42 +0530

    Update split-pdf.tsx

--- components/tools/split-pdf.tsx
+++ components/tools/split-pdf.tsx
@@ -28,7 +28,7 @@ export default function SplitPdf() {
       copiedPages.forEach((page) => newPdf.addPage(page));
 
       const pdfBytes = await newPdf.save();
-      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+     const blob = new Blob([pdfBytes as unknown as Uint8Array<ArrayBuffer>], { type: 'application/pdf' });
       const url = URL.createObjectURL(blob);
       const a = document.createElement("a");
       a.href = url;
