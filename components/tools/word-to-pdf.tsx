bec83c3a90067bd930d39f3f8c5c73ad974b9a19
vasudevsarswat48-del <vasudevsarswat48@gmail.com>
2026-08-12 21:45:44 +0530

    Update word-to-pdf.tsx

--- components/tools/word-to-pdf.tsx
+++ components/tools/word-to-pdf.tsx
@@ -112,8 +112,8 @@ export default function WordToPdf() {
       }
 
       const pdfBytes = await pdfDoc.save();
-      const blob = new Blob([pdfBytes], { type: "application/pdf" });
-      const url = URL.createObjectURL(blob);
+     const pdfBytes = await pdfDoc.save();
+const blob = new Blob([pdfBytes.buffer as ArrayBuffer], { type: "application/pdf" });
 
       setConvertedPdfUrl(url);
     } catch (error) {
