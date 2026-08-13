1012495c3d35ae1f558e873e21f5c3628b8b3a19
vasudevsarswat48-del <vasudevsarswat48@gmail.com>
2026-08-13 19:06:49 +0530

    Update invoice-generator.tsx

--- components/tools/invoice-generator.tsx
+++ components/tools/invoice-generator.tsx
@@ -152,7 +152,6 @@ export default function TaxInvoiceGenerator() {
       ? "bg-amber-600"
       : "bg-red-600";
 
-  const handlePrint = () => window.print();
 
   return (
     <div className="max-w-4xl mx-auto p-4 space-y-4">
